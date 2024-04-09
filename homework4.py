f383099094016179d4e335fcdc46aa9006e783e9
ddutov <165052103+ddutov@users.noreply.github.com>
2024-04-09 18:49:44 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,9 @@
+def print_params(arg):
+    print(arg, arg)
+
+
+print_params('mama')
+print_params(['jun', 'jul', 'aug'])
+print_params(True)
+print_params(777)
+print_params({1: 'Su', 3: 'Tu', 5:'th'})
